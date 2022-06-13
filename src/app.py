b40e7ce450f188d2635c8b17c7ac53d4a6f72dd3
kaiserone01 <mrporteles@gmail.com>
2022-06-13 13:06:48 +0000

    ¨cambios¨

---
+++ src/app.py
@@ -0,0 +1,70 @@
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+import os
+from flask import Flask, request, jsonify, url_for
+from flask_cors import CORS
+from utils import APIException, generate_sitemap
+from datastructures import FamilyStructure
+#from models import Person
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+CORS(app)
+
+# create the jackson family object
+jackson_family = FamilyStructure("Jackson")
+
+# Handle/serialize errors like a JSON object
+@app.errorhandler(APIException)
+def handle_invalid_usage(error):
+    return jsonify(error.to_dict()), error.status_code
+
+# generate sitemap with all your endpoints
+@app.route('/')
+def sitemap():
+    return generate_sitemap(app)
+
+@app.route('/members', methods=['GET'])
+def handle_hello():
+
+    # this is how you can use the Family datastructure by calling its methods
+    members = jackson_family.get_all_members()
+    return jsonify(members), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.json
+    if member is None:
+        return jsonify({"message":"Invalid name"}), 400
+    jackson_family.add_member(member)
+    return jsonify({"message":"Family member added succesfully"}), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    member = jackson_family.delete_member(member_id)
+    if member is None:
+        return jsonify({"message":"ID doesn't exist"}), 400
+    return jsonify(member), 200
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    member = jackson_family.get_member(member_id)
+    if member is None:
+        return jsonify({"message":"Family member doesn't exist"}), 400
+    return jsonify(member), 200
+
+@app.route('/member/<int:member_id>', methods=['PUT'])
+def update_member(member_id):
+    member = request.json
+    jackson_family.update_member(member_id, member)
+    if member is None:
+        return jsonify({"message":"Invalid name"}), 400
+    return jsonify(member), 200
+
+
+
+# this only runs if `$ python src/app.py` is executed
+if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
